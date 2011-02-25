6c1ebb00dcb0e0a44c1f209f007810b54fe2b249
daniela.balmus <daniela.balmus@8824bb7a-e99f-21a2-3026-790216ff4fc6>
2011-02-25 21:05:38 +0000

    changed unigram

--- src/lm/Test.java
+++ src/lm/Test.java
@@ -1,6 +1,9 @@
 package lm;
 
+import java.util.HashMap;
+
 import lm.models.BigramModel;
+import lm.models.UnigramModel;
 
 // That looks good
 public class Test {
@@ -9,11 +12,17 @@ public class Test {
 	 * @param args
 	 */
 	public static void main(String[] args) {
-		
-		String[] filecontent = FileRead.fileRead("Test.txt");
-		for(int i=0; i< filecontent.length; i++)
+
+		String[] filecontent = FileRead.fileRead("Dataset3/Test.txt");
+		for (int i = 0; i < filecontent.length; i++)
 			System.out.println(filecontent[i]);
+
+		UnigramModel unigramModel = new UnigramModel();
+		HashMap<String, Integer> unigram = unigramModel
+				.calculateUnigramModel(filecontent);
 		
+		
+
 		String text = "I am Sam Sam I am I do not like green eggs and ham";
 		String[] testCorpus = text.split(" ");
 		String[] vocabulary = "I am Sam do not like green eggs and ham"
@@ -21,8 +30,6 @@ public class Test {
 
 		BigramModel bigramModel = new BigramModel(vocabulary, testCorpus);
 		System.out.println(bigramModel.toString());
-		
-		
 
 	}
 }
--- src/lm/models/UnigramModel.java
+++ src/lm/models/UnigramModel.java
@@ -1,58 +1,86 @@
 package lm.models;
-import java.util.HashMap;
-import java.util.Iterator;
-import java.util.Set;
-import java.io.*;
 
+import java.io.BufferedWriter;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.util.HashMap;
 
 public class UnigramModel {
 
-	
-	public void calculateUnigramModel(String[] input){
-		
+	public HashMap<String, Integer> calculateUnigramModel(String[] input) {
+
 		int size = input.length;
-		
-		HashMap<String,Integer> output = new HashMap<String,Integer>();
-		
-		for(int count = 0; count < size; count++){
-		
+
+		HashMap<String, Integer> output = new HashMap<String, Integer>();
+
+		for (int count = 0; count < size; count++) {
+
 			String temp = input[count];
-			
-			if(!output.containsKey(temp)){
+
+			if (!output.containsKey(temp)) {
 				output.put(temp, new Integer(1));
-			}
-			else {
-				
+			} else {
+
 				Integer occ = output.get(temp);
-				int noc = occ.intValue()+1;
+				int noc = occ.intValue() + 1;
 				output.put(temp, new Integer(noc));
 			}
-			
+
 		}
+
+		return output;
+	}
+
+	public void writeUnigramModelToFile(HashMap<String, Integer> output) {
+		BufferedWriter bufferedWriter = null;
+		try {
+			// FileOutputStream fstream = new FileOutputStream("unigram.txt");
+			// DataOutputStream out = new DataOutputStream(fstream);
+			// Set<String> keys = output.keySet();
+			// Iterator<String> iter = keys.iterator();
+			//	        
+			// while(iter.hasNext()){
+			// String s = iter.next();
+			// System.out.print(s);
+			// System.out.print(" ");
+			// int i = output.get(s).intValue();
+			// System.out.println(i);
+			//	           
+			// out.writeChars(s);
+			// out.writeChars(" ");
+			// out.writeInt(i);
+			// out.writeChars("\n");
+			// }
+			// out.close();
+			bufferedWriter = new BufferedWriter(new FileWriter("unigram.txt"));
+
+			for (String word : output.keySet()) {
+				bufferedWriter.write(word + " " + output.get(word));
+				bufferedWriter.newLine();
+			}
+
+		} catch (Exception e) {
+			System.err.println("Error: " + e.getMessage());
+		} finally {
+			if (bufferedWriter != null) {
+				try {
+					bufferedWriter.flush();
+					bufferedWriter.close();
+				} catch (IOException e) {
+					e.printStackTrace();
+				}
+			}
+		}
+	}
+
+	public double getTokenCount(HashMap<String, Integer> unigram) {
+		double tokenCount = 0;
 		
-		try{
-			FileOutputStream fstream = new FileOutputStream("unigram.txt");
-			DataOutputStream  out = new DataOutputStream(fstream);
-	        Set<String> keys = output.keySet();
-	        Iterator<String> iter = keys.iterator();
-	        
-	        while(iter.hasNext()){
-	           String s = iter.next();
-	           System.out.print(s);
-	           System.out.print(" ");
-	           int i = output.get(s).intValue();
-	           System.out.println(i);
-	           
-	           out.writeChars(s);
-	           out.writeChars(" ");
-	           out.writeInt(i);
-	           out.writeChars("\n");
-	        }
-	        out.close();
-				
-        }catch(Exception e){
-           System.err.println("Error: " + e.getMessage());
-        }
+		for(String word : unigram.keySet()){
+			tokenCount += unigram.get(word);
+		}
+
+		return tokenCount;
+
 	}
 }
-
