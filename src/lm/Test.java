2e171a359ecc1af01822d2484d4ed984b9beba45
daniela.balmus <daniela.balmus@8824bb7a-e99f-21a2-3026-790216ff4fc6>
2011-02-26 17:32:25 +0000

    Add generateNextWord and generateNextBigram

--- src/lm/Test.java
+++ src/lm/Test.java
@@ -2,6 +2,8 @@ package lm;
 
 import lm.models.BigramModel;
 import lm.models.UnigramModel;
+import lm.objects.Bigram;
+import lm.objects.Unigram;
 import lm.ext.*;
 
 public class Test {
@@ -10,29 +12,45 @@ public class Test {
 	 * @param args
 	 */
 	public static void main(String[] args) {
-		
-		String[] filecontent = FileRead.fileRead("Test.txt");
-		for(int i=0; i< filecontent.length; i++)
-			System.out.println(filecontent[i]);
-		
-		System.out.println("----------------------------------------------");
-		String text = "I am Sam Sam I am I do not like green eggs and ham";
-		String[] testCorpus = text.split(" ");
-		String[] vocabulary = "I am Sam do not like green eggs and ham"
-				.split(" ");
 
-		BigramModel bigramModel = new BigramModel(vocabulary, testCorpus);
-		System.out.println(bigramModel.toString());
-		
+		String[] filecontent = FileRead.fileRead("Dataset3/Test.txt");
+		for (int i = 0; i < filecontent.length; i++)
+			System.out.println(filecontent[i]);
+
+		// System.out.println("----------------------------------------------");
+		// String text = "I am Sam Sam I am I do not like green eggs and ham";
+		// String[] testCorpus = text.split(" ");
+		// String[] vocabulary = "I am Sam do not like green eggs and ham"
+		// .split(" ");
+
 		System.out.println("----------------------------------------------");
 		UnigramModel unigramModel = new UnigramModel(filecontent);
 		System.out.println(unigramModel.toString());
-		
+
 		System.out.println("----------------------------------------------");
-		SpellChecker spellchecker = new SpellChecker("Test.txt");
-		System.out.println(spellchecker.correct("womed"));
-		
-		
+		BigramModel bigramModel = new BigramModel(unigramModel
+				.getUnigramModel(), filecontent);
+		bigramModel.setProbabilities(unigramModel.getUnigramModel());
+		System.out.println(bigramModel.toString());
+		System.out.println("----------------------------------------------");
+
+		for (int i = 0; i < 10; i++) {
+			Unigram unigram = unigramModel.generateNextWord();
+			System.out.print(unigram.getWord() + ' ');
+		}
+		System.out.print("\n");
+		System.out.println("----------------------------------------------");
+		String prefix = "When";
+		System.out.print(prefix + ' ');
+		for (int i = 0; i < 10; i++) {
+			Bigram bigram = bigramModel.generateNextBigram(prefix);
+			prefix = bigram.getSecond();
+			System.out.print(bigram.getSecond() + ' ');
+		}
+
+		// System.out.println("----------------------------------------------");
+		// SpellChecker spellchecker = new SpellChecker("Test.txt");
+		// System.out.println(spellchecker.correct("womed"));
 
 	}
 }
